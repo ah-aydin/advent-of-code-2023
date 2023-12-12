7ff92db21a0cf3d2f02da260721c12bcf90f9ef0
Ahmed Hamza Aydin <ahamzaaydin.py@gmail.com>
2023-12-12 19:52:05 +0100

    Problem 12, part2. The guy who wrote the memoize crate, life saver.

--- problem_12/src/main.rs
+++ problem_12/src/main.rs
@@ -1,8 +1,10 @@
+use memoize::memoize;
 use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-fn find_arrangements(spring: &str, mut group_sizes: Vec<usize>, tab: usize) -> u64 {
+#[memoize]
+fn find_arrangements(spring: String, mut group_sizes: Vec<usize>, tab: usize) -> u64 {
     if spring.is_empty() {
         return match group_sizes.is_empty() {
             true => 1,
@@ -21,9 +23,9 @@ fn find_arrangements(spring: &str, mut group_sizes: Vec<usize>, tab: usize) -> u
     let first_c = spring.chars().nth(0).unwrap();
 
     s += match first_c {
-        '.' => find_arrangements(&spring[1..], group_sizes.clone(), tab + 1),
+        '.' => find_arrangements(spring[1..].to_string(), group_sizes.clone(), tab + 1),
         '?' => {
-            let mut s = find_arrangements(&spring[1..], group_sizes.clone(), tab + 1);
+            let mut s = find_arrangements(spring[1..].to_string(), group_sizes.clone(), tab + 1);
             if group_size <= spring.len()
                 && !spring[..group_size].contains('.')
                 && spring.chars().nth(group_size).unwrap_or('.') != '#'
@@ -32,7 +34,8 @@ fn find_arrangements(spring: &str, mut group_sizes: Vec<usize>, tab: usize) -> u
                 let next_spring = match group_size + 1 < spring.len() {
                     true => &spring[(group_size + 1)..],
                     false => "",
-                };
+                }
+                .to_string();
                 s += find_arrangements(next_spring, group_sizes.clone(), tab + 1);
             }
             s
@@ -47,7 +50,8 @@ fn find_arrangements(spring: &str, mut group_sizes: Vec<usize>, tab: usize) -> u
                 let next_spring = match group_size + 1 < spring.len() {
                     true => &spring[(group_size + 1)..],
                     false => "",
-                };
+                }
+                .to_string();
                 s += find_arrangements(next_spring, group_sizes.clone(), tab + 1);
             }
             s
@@ -80,13 +84,32 @@ fn main() {
             .map(|s| s.parse().unwrap())
             .collect();
 
-        println!("\n=======================================");
-        println!("Calculating for: {spring} {:?}", group_sizes);
-
-        let s = find_arrangements(&spring, group_sizes.clone(), 0);
-        println!("Sol: {}", s);
+        let s = find_arrangements(spring, group_sizes.clone(), 0);
         part1 += s;
     });
-
     println!("Part 1: {}", part1);
+
+    let mut part2: u64 = 0;
+    lines.iter().for_each(|line| {
+        let line_split: Vec<&str> = line.split(" ").collect();
+        let spring: String = line_split.get(0).unwrap().to_string();
+        let spring = format!("{}?{}?{}?{}?{}", spring, spring, spring, spring, spring);
+
+        let group_sizes: Vec<usize> = line_split
+            .get(1)
+            .unwrap()
+            .split(',')
+            .map(|s| s.parse().unwrap())
+            .collect();
+        let mut new_group_sizes: Vec<usize> = Vec::with_capacity(group_sizes.len() * 5);
+        for _ in 0..5 {
+            group_sizes
+                .iter()
+                .for_each(|group_size| new_group_sizes.push(*group_size));
+        }
+
+        let s = find_arrangements(spring, new_group_sizes.clone(), 0);
+        part2 += s;
+    });
+    println!("Part 2: {}", part2);
 }
